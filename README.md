c07f1923f6651e3f001fb0cc07943e9cf4a3f92d
Brian Mucio Duarte <106874869+brianmduarte@users.noreply.github.com>
2022-07-08 15:54:42 -0300

    description

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# project-to-do-list
-Projeto desenvolvido utilizando recursos do Javascript, HTML5 e CSS3.
+# Project-to-do-list 🔡
+Olá, Devs! Neste projeto busquei aplicar alguns conhecimentos que desenvolvi até o momento, indo um pouco além daquilo aprendido durantes as aulas. 
+
+Além disso, fiz o desenvolvimento de uma lista de tarefas, que além de permitir inserí-las, também é possível editar e removê-las. 
+
+Explorei bastante recurso do CSS para tornar o projeto mais bonito e interessantes para os usuários. 
+
+Segue o link para acesso: 
+
+<hr>
+
+Hi Devs! In this project I sought to apply some knowledge that I have developed so far, going a little beyond what I learned during classes.
+
+In addition, I developed a list of tasks, which in addition to allowing you to insert them, it is also possible to edit and remove them.
+
+I explored a lot of CSS features to make the project more beautiful and interesting for users.
+
+Here is the link for access:
+
+
---
+++ tarefas.js
@@ -0,0 +1,71 @@
+alert('Está funcionando!')
+
+window.addEventListener('load', () => {
+    const form = document.querySelector("#new-task-form");
+    const input = document.querySelector("#new-task-input");
+    const list_el = document.querySelector("#tasks");
+
+    form.addEventListener('submit' , (e) => {
+        e.preventDefault();
+
+        const task = input.value;
+        
+        if (!task) {
+            alert("Por favor, insira uma tarefa");
+            return;
+        }
+
+        const task_el = document.createElement("div");
+        task_el.classList.add("task");
+
+        const task_content_el = document.createElement("div");
+        task_content_el.classList.add("content");
+
+        task_el.appendChild(task_content_el);
+
+        const task_input_el = document.createElement("input");
+        task_input_el.classList.add("text");
+        task_input_el.type = "text";
+        task_input_el.value = task;
+        task_input_el.setAttribute("readonly", "readonly");
+
+        task_content_el.appendChild(task_input_el);
+
+       const task_actions_el = document.createElement("div");
+       task_actions_el.classList.add("actions");
+
+       const task_editar_el = document.createElement("button");
+       task_editar_el.classList.add("editar");
+       task_editar_el.innerHTML = "Editar";
+
+       const task_excluir_el = document.createElement ("button");
+       task_excluir_el.classList.add("excluir");
+       task_excluir_el.innerHTML = "Excluir";
+
+       task_actions_el.appendChild(task_editar_el);
+       task_actions_el.appendChild(task_excluir_el);
+
+       task_el.appendChild(task_actions_el);
+       
+       list_el.appendChild(task_el);
+
+       input.value = "";
+
+       task_editar_el.addEventListener('click' , () => {
+            if (task_editar_el.innerText.toLowerCase() == 
+            "editar") {
+                task_input_el.removeAttribute("readonly");
+                task_input_el.focus();
+                task_editar_el.innerText = "Save";
+            } else {
+                task_input_el.setAttribute("readonly", "readonly");
+                task_editar_el.innerText = "Editar";
+            }
+        });
+
+        task_excluir_el.addEventListener('click', () => {
+            list_el.removeChild(task_el);
+        });
+
+    })
+})
