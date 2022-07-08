3f87167e6493d984b6f67463e2d150dc7f3da0d0
Brian Mucio Duarte <106874869+brianmduarte@users.noreply.github.com>
2022-07-08 16:07:51 -0300

    Update tarefas.js

--- tarefas.js
+++ tarefas.js
@@ -1,5 +1,3 @@
-alert('Seja bem vindo ao meu projeto!')
-
 window.addEventListener('load', () => {
     const form = document.querySelector("#new-task-form");
     const input = document.querySelector("#new-task-input");
